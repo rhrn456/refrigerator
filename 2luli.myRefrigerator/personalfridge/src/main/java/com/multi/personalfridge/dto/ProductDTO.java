f474e2b61f0904d94ee40d07f8494de3370b9037
kimyoolim <muyl09797@gmail.com>
2024-03-05 15:33:30 +0900

    productDTO

--- 2luli.myRefrigerator/personalfridge/src/main/java/com/multi/personalfridge/dto/ProductDTO.java
+++ 2luli.myRefrigerator/personalfridge/src/main/java/com/multi/personalfridge/dto/ProductDTO.java
@@ -1,5 +1,25 @@
 package com.multi.personalfridge.dto;
 
-public class ProductDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ProductDTO {
+	private int productid;
+	private String productname;
+	private String productcontent;
+	private String productquantity;
+	private int productprice;
+	private String productimg;
+	private String appenddate;
+	private String limitdate;
+	private String productcategory;
+	
 }
