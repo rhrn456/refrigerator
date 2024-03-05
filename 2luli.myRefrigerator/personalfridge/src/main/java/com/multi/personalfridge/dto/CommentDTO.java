be251f4e290288ddb61c4010f616e8187826d8fd
kimyoolim <muyl09797@gmail.com>
2024-03-05 15:30:42 +0900

    commentDTO

--- 2luli.myRefrigerator/personalfridge/src/main/java/com/multi/personalfridge/dto/CommentDTO.java
+++ 2luli.myRefrigerator/personalfridge/src/main/java/com/multi/personalfridge/dto/CommentDTO.java
@@ -1,12 +1,21 @@
 package com.multi.personalfridge.dto;
 
-import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class CommentDTO {
 	
 	private int comment_no;				
-	private int board_id;			
+	private int boardid;			
 	private String c_content;
-	private String reg_date;
-	private String user_id;
+	private String regdate;
+	private String userid;
 }
