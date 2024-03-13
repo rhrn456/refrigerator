97952c2a277ceb1aa59c725211d5a7fd35fb45ff
yhj1017 <2000.tony.yang@gmail.com>
2024-03-13 09:52:49 +0900

    마이페이지 유저 컨트롤러

--- 2luli.myRefrigerator/personalfridge/src/main/java/com/multi/personalfridge/mypage/MypageController.java
+++
@@ -1,8 +0,0 @@
-package com.multi.personalfridge.mypage;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MypageController {
-
-}
--- 2luli.myRefrigerator/personalfridge/src/main/java/com/multi/personalfridge/user/UserController.java
+++ 2luli.myRefrigerator/personalfridge/src/main/java/com/multi/personalfridge/user/UserController.java
@@ -3,6 +3,7 @@ package com.multi.personalfridge.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -70,4 +71,26 @@ public class UserController {
 //    	return userService.getUserById(userId);
 //    }
 	
+ // 마이페이지 조회
+    @GetMapping("/mypage")
+    public ModelAndView myPage(@RequestParam("userId") String userId) {
+        ModelAndView mav = new ModelAndView("mypage");
+        UserDTO user = userService.getUserById(userId);
+        mav.addObject("user", user);
+        return mav;
+    }
+
+    // 마이페이지 정보 업데이트
+    @PostMapping("/mypage/update")
+    public String updateMyPage(@ModelAttribute UserDTO user) {
+        userService.updateUser(user.getUser_id(), user);
+        return "redirect:/mypage?userId=" + user.getUser_id();
+    }
+
+    // 마이페이지 계정 삭제
+//    @PostMapping("/mypage/delete")
+//    public String deleteAccount(@RequestParam("userId") String userId) {
+//        userService.deleteUser(userId, password);
+//        return "redirect:/login";
+//    }
 }
