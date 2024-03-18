efbaec4a360d300220a1d8f1c86ca86eab901957
kimyoolim <muyl09797@gmail.com>
2024-03-18 14:00:23 +0900

    recipe 목록/상세페이지

--- 2luli.myRefrigerator/personalfridge/src/main/java/com/multi/personalfridge/recipe/RecipeController.java
+++ 2luli.myRefrigerator/personalfridge/src/main/java/com/multi/personalfridge/recipe/RecipeController.java
@@ -1,6 +1,8 @@
 package com.multi.personalfridge.recipe;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,31 +11,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.multi.personalfridge.dto.PageRequestDTO;
 import com.multi.personalfridge.dto.ProductDTO;
 import com.multi.personalfridge.dto.RecipeDTO;
 import com.multi.personalfridge.dto.RecipeProductDTO;
 import com.multi.personalfridge.product.ProductService;
 
 @Controller
+@RequestMapping("recipe")
 public class RecipeController {
+
+	private final RecipeService recipeService;
+	private final ProductService productService;
+	private final RecipeProductService recipeProductService;
+	
 	@Autowired
-	RecipeService recipeservice;
+	public RecipeController(RecipeService recipeService,
+			ProductService productService,
+			RecipeProductService recipeProductService) {
+		this.recipeService = recipeService;
+		this.productService = productService;
+		this.recipeProductService = recipeProductService;
+	}
 	
 	
-	@RequestMapping("/test")
+	
+	@GetMapping("/test")
 	public void test() {
 		//WEB-INF/views/test.jsp
 	}
 	
+	//insertRecipe
+	//recipe/insertRecipe
 	@RequestMapping("/insertRecipe")
 	public void insertRecipe(RecipeDTO recipeDTO) {
 		//System.out.println(recipeDTO);
-		boolean insertrecipe = recipeservice.insertRecipe(recipeDTO);
-		//WEB-INF/views/insertInit.jsp
+		boolean insertrecipe = recipeService.insertRecipe(recipeDTO);
+		//WEB-INF/views/recipe/insertInit.jsp
 		System.out.println(insertrecipe);
 	}
 	
@@ -41,32 +60,160 @@ public class RecipeController {
 	//레시피목록 
 	@GetMapping("/recipe")
 	public ModelAndView getRecipeList(ModelAndView mv) {
-		List<RecipeDTO> recipeList = recipeservice.recipeList();
+		List<RecipeDTO> recipeList = recipeService.recipeList();
 		mv.setViewName("recipeList");
 		mv.addObject("recipeList", recipeList);
 		
 		return mv;
 	}
 	
+	//레시피샵목록확인
+	@RequestMapping("/recipeshop")
+	public void recipeshop() {
+		//WEB-INF/views/recipe/recipeshop.jsp
+	}
+	
+	@GetMapping("/recipeshop")
+	public String getRecipe(Model model) {
+		List<RecipeDTO> recipe = recipeService.getAllrecipe();
+		model.addAttribute("recipe", recipe);
+		return "recipe/recipeshop";
+	}
+	
+	
+	//레시피 상세페이지
+//	@RequestMapping("/recipedetail")
+//	public void recipedetail() {
+//		//WEB-INF/views/recipe/recipedetail.jsp
+//	}
+	
+	@GetMapping("/recipedetail")
+	public String getRecipeById(int recipe_id, Model model) {
+		System.out.println(recipe_id);
+		RecipeDTO recipe = recipeService.getRecipeById(recipe_id);
+		model.addAttribute("recipe", recipe);
+		return "recipe/recipedetail";
+	}
+
+//	//리스트페이지
+//	@GetMapping("/recipeshop")
+//	public String RecipeListAdmin(@RequestParam(defaultValue ="1") int page,Model model) {
+//		int pageSize = 8;
+//		List<RecipeDTO> recipelist = recipeService.getAllRecipePage(page, pageSize);
+//		System.out.println(recipelist);
+//		System.out.println(recipelist.size());
+//		int totalRecipe = recipelist.size();
+//		int totalPages = (int) Math.ceil((double) totalRecipe / pageSize); 
+//		if(totalPages >5) {
+//			totalPages = 5;
+//		}
+//	    PageRequestDTO pageRequestDTO = new PageRequestDTO().builder()
+//										.total(totalRecipe)
+//										.pageAmount(totalPages)
+//										.currentPage(page)
+//										.amount(pageSize)
+//										.build();
+//		model.addAttribute("recipelist",recipelist);
+//		model.addAttribute("pageInfo", pageRequestDTO);
+//		return "recipe/recipeshop";
+//	}
+//	
+	//레시피 전체/카테고리 페이징
+	@GetMapping("/getRecipeByCategory")
+	@ResponseBody
+	public Map<String, Object> getRecipeByCategory(@RequestParam String category, @RequestParam int page, @RequestParam int pageSize) {
+	    System.out.println(category+" " + page + " " + pageSize);
+		Map<String, Object> parameters = new HashMap<>();
+	    List<RecipeDTO> recipeList = recipeService.getRecipesByCategory(category);
+	    List<RecipeDTO> recipes = recipeService.getRecipesByCategoryAndPage(category, page, pageSize);
+	    int totalRecipes = recipeList.size();  
+	    int totalPages = (int) Math.ceil((double) totalRecipes / pageSize); 
+		if(totalPages >5) {
+			totalPages = 5;
+		}
+	    PageRequestDTO pageRequestDTO = new PageRequestDTO().builder()
+										.total(totalRecipes)
+										.pageAmount(totalPages)
+										.currentPage(page)
+										.amount(pageSize)
+										.build();
+	    parameters.put("recipes", recipes);
+	    parameters.put("pageInfo", pageRequestDTO);
+	    return parameters;
+	}
+
+	//레시피 검색/페이징
+	@GetMapping("/searchRecipe")
+	@ResponseBody
+	public Map<String, Object> getRecipeBySearch(@RequestParam String category, @RequestParam String keyword, @RequestParam(defaultValue ="1") int page, @RequestParam int pageSize) {
+	    Map<String, Object> parameters = new HashMap<>();
+	    List<RecipeDTO> recipes = recipeService.getRecipesBykeywordAndPage(category, keyword, page, pageSize);
+	    List<RecipeDTO> recipeList = recipeService.getRecipesBykeyword(category, keyword);
+	    int totalRecipes = recipeList.size();
+	    int totalPages = (int) Math.ceil((double) totalRecipes / pageSize); 
+		if(totalPages >5) {
+			totalPages = 5;
+		}
+	    PageRequestDTO pageRequestDTO = new PageRequestDTO().builder()
+										.total(totalRecipes)
+										.pageAmount(totalPages)
+										.currentPage(page)
+										.amount(pageSize)
+										.build();  
+	    parameters.put("recipes", recipes);
+	    parameters.put("pageInfo", pageRequestDTO);
+	    return parameters;
+	}
+	
+	
+	//화면이동
 
 
 	
-	
-	
+//	---------레시피 추가---------------------
 
-	private final RecipeService recipeService;
-	private final ProductService productService;
-	private final RecipeProductService recipeProductService;
-
-    @Autowired
-    public RecipeController(RecipeService recipeService,
-    						ProductService productService,
-    						RecipeProductService recipeProductService) {
-    	this.recipeService = recipeService;
-        this.productService = productService;
-        this.recipeProductService = recipeProductService;
-    }
-    
+	@GetMapping("/RecipePlus")
+	public String insertRecipe(@ModelAttribute RecipeDTO recipe, Model model) {
+		System.out.println(recipe);
+	    boolean result = recipeService.insertRecipe(recipe);
+	    RecipeDTO onerecipe = recipeService.getReturnInsertRecipe(recipe.getRecipe_name());
+	    List<ProductDTO> productList = productService.getFullProduct();
+	    System.out.println(productList);
+	    System.out.println(productList.size());
+	    System.out.println(onerecipe);
+	    if (result) {
+	    	model.addAttribute("onerecipe",onerecipe);
+	    	model.addAttribute("productList",productList);
+	        return "admin/plusrecipeproducts";
+	    }
+	    return "error";
+	}
 	
+	@GetMapping("/RecipeProductPlus")
+	public String PlustRecipeProducts(@RequestParam("recipeProducts") String recipeProductsJson) { 
+	    ObjectMapper objectMapper = new ObjectMapper(); 
+	    System.out.println(recipeProductsJson); 
+	    
+	    try {
+	        // JSON 문자열을 RecipeProductDTO 배열로 변환
+	        RecipeProductDTO[] recipeProducts = objectMapper.readValue(recipeProductsJson, RecipeProductDTO[].class);
+	        
+	        // 모든 제품을 처리하기 위해 루프 사용
+	        for (RecipeProductDTO product : recipeProducts) {
+	            // 변경된 데이터를 recipeProductService에 전달
+	            boolean result = recipeProductService.PlustRecipeProducts(product);
+	            if (!result) {
+	                // 처리 중 문제 발생 시 에러 반환
+	                return "error";
+	            }
+	        }
+	        // 모든 제품이 성공적으로 처리된 경우에만 성공 반환
+	        return "redirect:/admin";
+	    } catch (JsonProcessingException e) {
+	        // JSON 파싱에 실패한 경우에 대한 예외 처리
+	        e.printStackTrace();
+	        return "error";
+	    }
+	}
 
 }
---
+++ 2luli.myRefrigerator/personalfridge/src/main/webapp/WEB-INF/views/recipe/test.jsp
@@ -0,0 +1,107 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+<script type="text/javascript">
+$(function() { //jquery - js의 라이브러리 
+	$('#b1').click(function() { //이벤트가 발생하면 자동호출되는 익명함수(콜백함수)
+		$.ajax({
+			url : "http://openapi.foodsafetykorea.go.kr/api/67d21b2aa52f435480ad/COOKRCP01/json/291/300",
+			success: function(json) {
+				
+				//RCP_NM : 메뉴명 //recipe_name
+				//ATT_FILE_NO_MAIN : 이미지명 //recipe_img
+				//RCP_WAY2 : 조리방법	
+				//RCP_PAT2 : 요리종류 //recipe_category
+				//INFO_WGT : 중량(1인분)
+				//INFO_ENG : 열량 //nutrition_facts
+				//INFO_CAR : 탄수화물 //nutrition_facts
+				//INFO_PRO : 단백질 //nutrition_facts
+				//INFO_FAT : 지방 //nutrition_facts
+				//INFO_NA : 나트륨 //nutrition_facts
+				//RCP_PARTS_DTLS : 재료정보 //ingredient
+				//MANUAL01 ~ 20 : 만드는법 //recipe_content
+				
+				/* let recipe_name = json.COOKRCP01.row[0].RCP_NM;
+				let recipe_img = json.COOKRCP01.row[0].ATT_FILE_NO_MAIN;
+				let recipe_category = json.COOKRCP01.row[0].RCP_PAT2;
+				let nutrition_facts = "열량 : " + json.COOKRCP01.row[0].INFO_ENG + "탄수화물 : " +json.COOKRCP01.row[0].INFO_CAR+
+									"단백질 : "+ 	json.COOKRCP01.row[0].INFO_PRO+ " 지방 : "+json.COOKRCP01.row[0].INFO_FAT+
+									"나트륨 : "+json.COOKRCP01.row[0].INFO_NA;
+				let ingredient = json.COOKRCP01.row[0].RCP_PARTS_DTLS;
+				let recipe_content = json.COOKRCP01.row[0].MANUAL01+json.COOKRCP01.row[0].MANUAL02+
+									json.COOKRCP01.row[0].MANUAL03;
+				
+				console.log(recipe_name);
+				console.log(recipe_img);
+				console.log(recipe_category);
+				console.log(nutrition_facts);
+				console.log(ingredient);
+				console.log(recipe_content); */
+				
+				let data = json.COOKRCP01.row;
+				
+				for(let i = 0; i< data.length; i++){
+					/* let recipe_name = data[i].RCP_NM;
+					let recipe_img = data[i].ATT_FILE_NO_MAIN;
+					let recipe_category = data[i].RCP_PAT2;
+					let nutrition_facts = "열량 : " + data[i].INFO_ENG + "탄수화물 : " +data[i].INFO_CAR+
+										"단백질 : "+ 	data[i].INFO_PRO+ " 지방 : "+ data[i].INFO_FAT+
+										"나트륨 : "+data[i].INFO_NA;
+					let ingredient = data[i].RCP_PARTS_DTLS;
+					let recipe_content = data[i].MANUAL01+data[i].MANUAL02+ data[i].MANUAL03; */
+					let recipe_name = json.COOKRCP01.row[i].RCP_NM;
+					let recipe_img = json.COOKRCP01.row[i].ATT_FILE_NO_MAIN;
+					let recipe_category = json.COOKRCP01.row[i].RCP_PAT2;
+					let nutrition_facts = "열량 : " + json.COOKRCP01.row[i].INFO_ENG +""+ "탄수화물 : " +json.COOKRCP01.row[i].INFO_CAR+""+
+										"단백질 : "+ 	json.COOKRCP01.row[i].INFO_PRO+ "" + " 지방 : "+json.COOKRCP01.row[i].INFO_FAT+ "" +
+										"나트륨 : "+json.COOKRCP01.row[i].INFO_NA;
+					let ingredient = json.COOKRCP01.row[i].RCP_PARTS_DTLS;
+					let recipe_content = json.COOKRCP01.row[i].MANUAL01+json.COOKRCP01.row[i].MANUAL02+
+										json.COOKRCP01.row[i].MANUAL03;
+					
+					console.log(recipe_name);
+					console.log(recipe_img);
+					console.log(recipe_category);
+					console.log(nutrition_facts);
+					console.log(ingredient);
+					console.log(recipe_content);
+					$.ajax({
+						url:"insertRecipe",
+						type:"post",
+						data:{
+							recipe_name : recipe_name,
+							recipe_img : recipe_img,
+							recipe_category : recipe_category,
+							nutrition_facts : nutrition_facts,
+							ingredient : ingredient,
+							recipe_content : recipe_content
+						},
+						success: function() {
+						
+						}
+						
+					});
+				}
+				
+				
+			}//success
+		})//ajax
+	})//b1
+	
+	
+}) //$
+</script>
+</head>
+<body>
+<button id="b1">레시피</button>
+
+<hr color="red">
+<div id="result"></div>
+</body>
+</html>
